fd1679d8ca91ab7efa90d8eda87e4721052b1f37
nabechin <nabenabe0258@gmail.com>
2021-06-06 20:13:51 +0900

    setting webpack and tsconfig

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { App } from './App';
 
-ReactDom.render(<App />, document.createElement('div'));
+ReactDom.render(<App />, document.getElementById('body'));
---
+++ client/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/index.tsx',
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  devtool: 'inline-source-map',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{ test: /\.tsx?$/, loader: 'ts-loader' }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'html/index.html',
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    hot: true,
+    port: 3000,
+  },
+};
