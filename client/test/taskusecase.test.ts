218fd1bef68a2fc4514b91644fc9882e0fdeb35c
nabechin <nabenabe0258@gmail.com>
2021-06-23 22:48:45 +0900

    front usecase test

--- client/test/taskusecase.test.ts
+++ client/test/taskusecase.test.ts
@@ -10,10 +10,57 @@ class MockTaskRepository implements ITaskRepository {
 describe('TaskUsecase', () => {
   test('createTask', async () => {
     const mockTaskRepository = new MockTaskRepository();
+    const createTaskFormValue = { title: 'title' };
+    mockTaskRepository.createTask.mockReturnValue({
+      id: '123',
+      ...createTaskFormValue,
+    });
+    const taskUseCase = new TaskUseCase(mockTaskRepository);
+    const result = await taskUseCase.createTask(createTaskFormValue);
+    expect(mockTaskRepository.createTask).toHaveBeenCalledWith(
+      createTaskFormValue
+    );
+    expect(result).toEqual({
+      id: '123',
+      ...createTaskFormValue,
+    });
+  });
+
+  test('getTasks', async () => {
+    const mockTaskRepository = new MockTaskRepository();
     const tasks = [{ id: '1', title: 'title' }];
     mockTaskRepository.getTasks.mockReturnValue(tasks);
     const taskUseCase = new TaskUseCase(mockTaskRepository);
     const results = await taskUseCase.getTasks();
     expect(results).toEqual(tasks);
+  });
+
+  test('deleteTask', async () => {
+    const mockTaskRepository = new MockTaskRepository();
+    const id = '123';
+    mockTaskRepository.deleteTask.mockReturnValue(undefined);
+    const taskUseCase = new TaskUseCase(mockTaskRepository);
+    taskUseCase.deleteTask(id);
+    expect(mockTaskRepository.deleteTask).toHaveBeenCalledWith(id);
+  });
+
+  test('filterTask', async () => {
+    const mockTaskRepository = new MockTaskRepository();
+    const filterTask = { title: 'title' };
+    mockTaskRepository.filterTasks.mockReturnValue([
+      {
+        id: '123',
+        ...filterTask,
+      },
+    ]);
+    const taskUseCase = new TaskUseCase(mockTaskRepository);
+    const results = await taskUseCase.filterTask(filterTask);
+    expect(mockTaskRepository.filterTasks).toHaveBeenCalledWith(filterTask);
+    expect(results).toEqual([
+      {
+        id: '123',
+        ...filterTask,
+      },
+    ]);
   });
 });
