4dbedf991630b548af7dd24d638abb76b662fa82
nabechin <nabenabe0258@gmail.com>
2021-06-25 23:43:41 +0900

    test with redux

--- client/test/TaskList.test.tsx
+++ client/test/TaskList.test.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { render, cleanup, screen } from '@testing-library/react';
 import TaskList from '../src/components/organisms/TaskList';
+import taskReducer from '../src/reducers/taskReducer';
+import { TaskState } from '../src/state/types';
+import { TaskAction } from '../src/actions/types';
+
+function renderWithRedux(
+  component: JSX.Element,
+  {
+    initialState,
+    store = createStore<TaskState, TaskAction, unknown, unknown>(
+      taskReducer,
+      {}
+    ),
+  } = {}
+) {
+  return { ...render(<Provider store={store}>{component}</Provider>) };
+}
 
 describe('TaskList', () => {
   it('render TaskList Component', () => {
     const aaa = {
       id: '1',
       title: 'title',
-      deleteTask: (id: string) => {
-        return;
-      },
     };
-    render(<TaskList {...aaa} />);
+    renderWithRedux(<TaskList {...aaa} />);
+    screen.debug();
   });
 });
