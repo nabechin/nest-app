742c71e62110426c62a773ad10c4497f1540ddf2
nabechin <nabenabe0258@gmail.com>
2021-06-26 22:12:04 +0900

    provide tasksrepository

--- api/src/tasks/tasks.module.ts
+++ api/src/tasks/tasks.module.ts
@@ -3,12 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Task, TaskSchema } from './schemas/task.schema';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { TasksRepository } from './tasks.repository';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }]),
   ],
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [TasksService, TasksRepository],
 })
 export class TasksModule {}
