7a08b77753bec52f2e9f8e3a4f35b8b61a4963e7
nabechin <nabenabe0258@gmail.com>
2021-07-31 18:36:14 +0900

    set up swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: ['http://localhost:3000'] });
+  const config = new DocumentBuilder()
+    .setTitle('Tasks')
+    .setDescription('Tasks API description')
+    .setVersion('1.0')
+    .addTag('tasks')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(5000);
 }
 bootstrap();
